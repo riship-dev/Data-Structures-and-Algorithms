002857983e6b8201fcb21df919fa284b88c137fd
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2023-08-18 23:21:52 +0530

    First commit

---
+++ section02_EssentialCAndCppConcepts/023_practiceMonolithicProgram.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double length, breadth;
+    cout << "Enter length: "; cin >> length;
+    cout << "Enter breadth: "; cin >> breadth;
+    cout << "Area of " << length << " x " << breadth << " rectangle = " << length * breadth;
+}
