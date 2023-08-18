8c342f17b5d103f4c91e6a6c59a2da893b29a5c8
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2023-08-18 23:29:47 +0530

    First commit

---
+++ section02_EssentialCAndCppConcepts/024_practiceModularProgram.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double area(double l, double b);
+double perimeter(double l, double b);
+
+int main()
+{
+    double length, breadth;
+    cout << "Enter length: "; cin >> length;
+    cout << "Enter breadth: "; cin >> breadth;
+    cout << "Area of " << length << " x " << breadth << " rectangle = " << area(length, breadth) << endl;
+    cout << "Perimeter of " << length << " x " << breadth << " rectangle = " << perimeter(length, breadth);
+}
+
+double area(double l, double b)
+{
+    return l * b;
+}
+double perimeter(double l, double b)
+{
+    return 2 * (l + b);
+}
