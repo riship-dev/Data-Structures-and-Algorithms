7127a05437e2a9a273aa478320fa7e9937f1e375
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2023-08-31 23:45:05 +0530

    First commit

---
+++ section05_Recursion/052_letsCodeStaticAndGlobalInRecursion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fun(int n)
+{
+    static int x = 0;
+    if (n > 0)
+    {
+        x++;
+        return fun(n - 1) + x;
+    }
+    return 0;
+}
+
+int main()
+{
+    cout << fun(5);
+}
