4b41348586f530c7c198105f36a0bcb4e1551266
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2023-09-21 21:51:59 +0530

    First commit

---
+++ section07_arrayADT/109_getSetMaxSumAvg.cpp
@@ -0,0 +1,208 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array //using class to implement array as ADT
+{
+    private:
+        int *A; //array head pointer
+        int size; //size of array
+        int numberOfElements; //number of manually initialised elements
+    public:
+        Array() //constructor: Declares an array
+        {
+            cout << "<----------------Declaring array---------------->" << endl;
+            cout << "Size?: "; cin >> size;
+            A = new int[size];
+            cout << "Array declared." << endl;
+        }
+        void initialize() //function to manually initialize the array 
+        {
+            cout << "<----------------Initialising---------------->" << endl;
+            cout << "Number of Elements?: "; cin >> numberOfElements;
+            for(int i = 0; i < numberOfElements; i++)
+            {
+                cout << "A[" << i << "]?: "; cin >> A[i];
+            }
+            cout << "Array initialised. Array:" << endl;
+            display();
+        }
+        void display() //function to display all manually initialized elements in the array
+        {
+            //cout << "<----------------Printing---------------->" << endl;
+            for(int i = 0; i < numberOfElements; i++) cout << A[i] << " ";
+            cout << endl;
+            //cout << "All elements printed." << endl;  
+        }
+        void append() //function to append a user-input value to the end of the array
+        {
+            cout << "<----------------Appending---------------->" << endl;
+            int value; cout << "Value?: "; cin >> value;
+            A[numberOfElements] = value; //appending the array
+            numberOfElements++; //updating number of elements
+            cout << "Appended: " << value << endl;
+            cout << "Updated array:" << endl;
+            display();
+        }
+        void insert() //function to insert a user-input value at a specific index in the array
+        {
+            cout << "<----------------Inserting---------------->" << endl;
+            int index; cout << "Index?: "; cin >> index;
+            int value; cout << "Value?: "; cin >> value;
+            if(index >= 0 && index <= numberOfElements)
+            {
+                for(int i = numberOfElements; i > index; i--) A[i] = A[i - 1]; //copying i - 1 element to i element
+                A[index] = value; //inserting at given index
+                numberOfElements++; //updating number of elements
+                cout << "Inserted " << value << " at index " << index << endl;
+                cout << "Updated array:" << endl;
+                display();
+            }
+            else cout << "Invalid index :(" << endl;
+        }
+        void remove() //function to delete a user-input index's value
+        {
+            cout << "<----------------Deleting---------------->" << endl;
+            int index; cout << "Index?: "; cin >> index;
+            if(index >= 0 && index < numberOfElements)
+            {
+                for(int i = index; i < numberOfElements; i++) A[i] = A[i + 1]; //copy i + 1 elements to i
+                numberOfElements--;
+                cout << "Value at index " << index << " deleted." << endl;
+                cout << "Updated array:" << endl;
+                display();
+            }
+            else cout << "Invalid index :(" << endl;
+        }
+        void linearSearch() //function to search a user-input value linearly
+        {
+            cout << "<----------------Performing linear search---------------->" << endl;
+            int value; cout << "Value?: "; cin >> value;
+            bool notFound = 1;
+            for(int i = 0; i < numberOfElements; i++) if(A[i] == value) //traversing array to find a match
+            {
+                cout << value << " found at index " << i << endl;
+                notFound = 0;
+            }
+            if(notFound) cout << "Value not found :(" << endl;
+        }
+        void linearSearchTransposition() //linear search using transposition to improve efficiency
+        {
+            cout << "<----------------Performing linear search with transposition---------------->" << endl;
+            int value; cout << "Value?: "; cin >> value;
+            bool notFound = 1;
+            for(int i = 0; i < numberOfElements; i++) if(A[i] == value) //traversing array to find a match
+            {
+                cout << value << " found at index " << i << ". A[" << i << "] transposed."<< endl;
+                int temporary = A[i];
+                A[i] = A[i - 1];
+                A[i - 1] = temporary; 
+                notFound = 0;
+                cout << "Updated array: " << endl;
+                display();
+            }
+            if(notFound) cout << "Value not found :(" << endl;
+        }
+        void linearSearchMoveToHeader() //linear search using move to header to improve efficiency
+        {
+            cout << "<----------------Performing linear search with move to header---------------->" << endl;
+            int value; cout << "Value?: "; cin >> value;
+            bool notFound = 1;
+            for(int i = 0; i < numberOfElements; i++) if(A[i] == value) //traversing array to find a match
+            {
+                cout << value << " found at index " << i << ". A[" << i << "] moved to header."<< endl;
+                int temporary = A[i];
+                A[i] = A[0];
+                A[0] = temporary; 
+                notFound = 0;
+                cout << "Updated array: " << endl;
+                display();
+            }
+            if(notFound) cout << "Value not found :(" << endl;
+        }
+        void binarySearch() //function to search a user-input value using binary search
+        {
+            cout << "<----------------Performing binary search---------------->" << endl;
+            int value; cout << "Value?: "; cin >> value;
+            int first = 0, last = numberOfElements - 1;
+            while(first <= last)
+            {
+                int middle = (first + last) / 2;
+                if(A[middle] == value) 
+                {
+                    cout << value << " found at index " << middle << endl;
+                    break;
+                }
+                else if(middle < value) first = middle + 1;
+                else last = middle - 1;
+            }
+            if(first > last) cout << "Value not found :(" << endl;
+        }
+        void get() //function to get value of given index
+        {
+            cout << "<----------------Getting value---------------->" << endl;
+            int index; cout << "Index?: "; cin >> index;
+            if(index >= 0 && index < numberOfElements) cout << "Value at index " << index << ": " << A[index] << endl;
+            else cout << "Invalid index :(" << endl;
+        }
+        void set() //function to set given value to index
+        {
+            cout << "<----------------Setting value---------------->" << endl;
+            int index; cout << "Index?: "; cin >> index;
+            int value; cout << "Value?: "; cin >> value;
+            if(index >= 0 && index < numberOfElements) 
+            {
+                A[index] = value;
+                cout << value << " set to A[" << index << "]." << endl; 
+                cout << "Updated array:" << endl;
+                display();
+            }
+            else cout << "Invalid index :(" << endl;
+        }
+        void maximum() //function to find maximum of array
+        {
+            cout << "<----------------Finding maximum value in array---------------->" << endl;
+            int max = 0;
+            for(int i = 0; i < numberOfElements; i++) if(A[i] > max) max = A[i];
+            cout << "Maximum value: " << max << endl;
+        }
+        void minimum() //function to find minimum of array
+        {
+            cout << "<----------------Finding minimum value in array---------------->" << endl;
+            int min = A[0];
+            for(int i = 0; i < numberOfElements; i++) if(A[i] < min) min = A[i];
+            cout << "Minimum value: " << min << endl;
+        }
+        void sum() //function to find sum of values of array
+        {
+            cout << "<----------------Finding sum of values---------------->" << endl;
+            int sum = 0;
+            for(int i = 0; i < numberOfElements; i++) sum += A[i];
+            cout << "Sum = " << sum << endl;
+        }
+        void average() //function to find average of values of array
+        {
+            cout << "<----------------Finding average of values---------------->" << endl;
+            int sum = 0;
+            for(int i = 0; i < numberOfElements; i++) sum += A[i];
+            cout << "Average = " << (float) sum / numberOfElements << endl;
+        }
+};
+
+int main()
+{
+    Array array1;
+    array1.initialize();
+    //array1.append(); 
+    //array1.insert();
+    //array1.remove();
+    //array1.linearSearch();
+    //array1.linearSearchTransposition();
+    //array1.linearSearchMoveToHeader();
+    //array1.binarySearch();
+    array1.get();
+    array1.set();
+    array1.maximum();
+    array1.minimum();
+    array1.sum();
+    array1.average();
+}
