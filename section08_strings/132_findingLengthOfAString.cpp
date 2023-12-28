1be1f8b06f3d83c69d126df851828eea2e5681c3
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2023-12-28 09:17:47 +0530

    Improved modularity and added notes

--- section08_strings/132_findingLengthOfAString.cpp
+++ section08_strings/132_findingLengthOfAString.cpp
@@ -1,10 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int lenOf(char A[])
+{
+    int i = 0;
+    for(i = 0; A[i] != '\0'; i++);
+    return i;
+}
+
 int main()
 {
     char s[20]; int i;
     cout << "String?: "; gets(s);
-    for (i = 0; s[i] != '\0'; i++);
-    cout << "Size = " << i;
+    cout << lenOf(s);
 }
+
+/*
+Function Name:
+int lenOf(char A[])
+    The function is named lenOf.
+    It takes a character array A as input and returns an integer representing the length of the string.
+
+Length Calculation:
+int i = 0;
+for(i = 0; A[i] != '\0'; i++);
+    Initializes an integer i to 0, which will be used as a counter.
+    Enters a for loop that iterates through the elements of the character array A.
+    The loop continues as long as the character at index i of array A is not the null character '\0'.
+    Inside the loop:
+        Increments the counter i to track the length of the string.
+        Continues iterating until it encounters the null character '\0', which marks the end of the string.
+
+Returning the Length:
+return i;
+    After the loop ends, it returns the value of i, which represents the length of the string.
+
+Topics Covered:
+    String Traversal: Using a loop to go through each character in a string until the null character '\0' is encountered, marking the end of the string.
+    Loop Control: Using a for loop without an internal body (the loop is empty after the semicolon) to increment the counter until a condition is met.
+    String Length Determination: Counting the number of characters in a string by iterating through it and tracking the count until the null character is reached.
+*/
--- section08_strings/133_changingCaseOfAString.cpp
+++ section08_strings/133_changingCaseOfAString.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void changeCase(char A[]);
+
 int main()
 {
     char A[] = "WELcOME";
+    changeCase(A);
+}
+
+void changeCase(char A[])
+{
     int i = 0;
     for(i = 0; A[i] != '\0'; i++)
     {
@@ -12,3 +19,32 @@ int main()
     }
     cout << A;
 }
+
+/*
+Function Name:
+void changeCase(char A[])
+    The function is named changeCase.
+    It takes a character array A as input and doesn't return anything (void).
+
+Case Conversion:
+int i = 0;
+for(i = 0; A[i] != '\0'; i++)
+{
+    if(A[i] >= 62 && A[i] <= 90) A[i] += 32;
+    else if(A[i] >= 'a' && A[i] <= 122) A[i] -= 32;
+}
+    Initializes an integer i to 0, which will be used as a counter to traverse the string A.
+    Enters a for loop that iterates through each character in the string until it reaches the null character '\0'.
+    Inside the loop:
+        Checks if the current character is within the ASCII range of uppercase letters (>= 62 and <= 90). If so, it converts it to lowercase by adding 32 to its ASCII value.
+        If the character is within the ASCII range of lowercase letters (>= 'a' and <= 122), it converts it to uppercase by subtracting 32 from its ASCII value.
+
+Printing the Modified String:
+cout << A;
+    After modifying the case of characters in the string, it prints the updated string A.
+
+Topics Covered:
+    String Traversal: Using a loop to iterate through each character in a string until the null character '\0' is encountered.
+    ASCII Values: Manipulating characters based on their ASCII values to convert between uppercase and lowercase letters.
+    Conditional Statements: Using if-else conditions to check the range of ASCII values to determine whether to change the case of characters.
+*/
