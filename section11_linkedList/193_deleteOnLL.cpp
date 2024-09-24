69eb7af0fc9f7ac346aa6b942c91369fce35af2a
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-09-24 16:37:24 +0530

    Learn delete

---
+++ section11_linkedList/193_deleteOnLL.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+
+        Node (int data) {
+            this->data = data;
+            next = nullptr;
+        }
+};
+class LinkedList {
+    private:
+        Node *head;
+    public:
+        LinkedList() {
+            head = nullptr;
+        }
+        ~LinkedList() {
+            Node *current = head;
+            Node *nextNode = nullptr;
+            while (current) {
+                nextNode = current->next;
+                delete current;
+                current = nextNode;
+            }
+        }
+
+        void initialise(vector <int> values) {
+            head = new Node(values[0]);
+            Node *current = head;
+            for (int i = 1; i < values.size(); i++) {
+                current->next = new Node(values[i]);
+                current = current->next;
+            }
+        }
+        void display() {
+            Node *current = head;
+            while (current) {
+                cout << current->data << " ";
+                current = current->next;
+            }
+            cout << endl;
+        }
+
+        void deleteAtIndex(int index) {
+            if (index == 0) {
+                Node *toDelete = head;
+                head = head->next;
+                delete toDelete;
+                return;
+            }
+            Node *current = head;
+            for (int i = 1; i < index; i++) {
+                current = current->next;
+            }
+            Node* toDelete = current->next;
+            current->next = current->next->next;
+            delete toDelete;
+        }
+};
+
+int main() {
+    LinkedList list1;
+    list1.initialise({1, 2, 3, 4, 5});
+    list1.display();
+
+    list1.deleteAtIndex(4);
+    list1.display();
+}
