121a91484a143cebc2452bf9eca5c59f89ec85d9
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-09-30 16:46:45 +0530

    Code DLL

---
+++ section11_linkedList/216_doublyLinkedList_full.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        Node *previous;
+        int data;
+        Node *next;
+
+        Node (int value) {
+            data = value;
+            previous = nullptr;
+            next = nullptr;
+        }
+};
+
+int main() {
+    
+}
