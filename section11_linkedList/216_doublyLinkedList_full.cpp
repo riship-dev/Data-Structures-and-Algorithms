3dcca43cccf310c73423e2e4a2e306a4fb5e7032
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-09-30 21:26:00 +0530

    Code DLL test functions

--- section11_linkedList/216_doublyLinkedList_full.cpp
+++ section11_linkedList/216_doublyLinkedList_full.cpp
@@ -13,7 +13,53 @@ class Node {
             next = nullptr;
         }
 };
+class DoublyLinkedList {
+    private:
+        Node *head;
+    public:
+        DoublyLinkedList() {
+            head = nullptr;
+        }
+        ~DoublyLinkedList() {
+            Node *current = head, *toDelete = nullptr;
+            while (current) {
+                toDelete = current;
+                current = current->next;
+                delete toDelete;
+            }
+        }
+
+        void initialise(vector <int> values) {
+            head = new Node(values[0]);
+            Node *current = head;
+            for (int i = 1; i < values.size(); i++) {
+                current->next = new Node(values[i]);
+                current->next->previous = current;
+                current = current->next;
+            }
+        }
+        void display() {
+            Node *current = head;
+            while (current) {
+                cout << current->data << " ";
+                current = current->next;
+            }
+            cout << endl;
+        }
+        void displayInReverse() {
+            Node *current = head;
+            while (current->next) current = current->next;
+            while (current) {
+                cout << current->data << " ";
+                current = current->previous;
+            }
+            cout << endl;
+        }
+};
 
 int main() {
-    
+    DoublyLinkedList list1;
+    list1.initialise({1, 2, 3, 4, 5});
+    list1.display();
+    list1.displayInReverse();
 }
